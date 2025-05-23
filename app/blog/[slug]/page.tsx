55504e85be78a4ea071286cde4699f22c3c0cb50
wisdom6715 <olayiwolaibrahim46@gmail.com>
2025-05-23 17:51:33 +0100

    fixing redirection

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -29,6 +29,26 @@ export default function BlogPost() {
       slug: "multi-cloud-strategy-and-how-can-it-benefit-enterprises",
       url: "https://sudais.hashnode.dev/multi-cloud-strategy-and-how-can-it-benefit-enterprises",
     },
+    {
+      slug: "Mastering GitHub: A Beginner’s Guide to Code Hosting, Collaboration, and Version Control",
+      url: "https://sudais.hashnode.dev/mastering-github-a-beginners-guide-to-code-hosting-collaboration-and-version-control",
+    },
+    {
+      slug: "Cloud adoption in Nigeria’s banking: The key to a modern, inclusive future",
+      url: "https://businessday.ng/opinion/article/cloud-adoption-in-nigerias-banking-the-key-to-a-modern-inclusive-future/",
+    },
+    {
+      slug: "The Role of Excel in Business Intelligence and Data-Driven Decision-Making",
+      url: "https://sudais.hashnode.dev/the-role-of-excel-in-business-intelligence-and-data-driven-decision-making",
+    },
+    {
+      slug: "Predictive Analytics Failures in Business: What Went Wrong and Why",
+      url: "https://sudais.hashnode.dev/predictive-analytics-failures-in-business-what-went-wrong-and-why",
+    },
+    {
+      slug: "AI isn’t taking our jobs; it’s transforming them",
+      url: "https://businessday.ng/opinion/article/ai-isnt-taking-our-jobs-its-transforming-them/",
+    },
   ]
 
   const blogPost = blogPosts.find((post) => post.slug === slug)
